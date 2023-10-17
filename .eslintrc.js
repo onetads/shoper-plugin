9826f89b2b71edab1ecbe014d09d6c4cd5c0af88
Przemysław Czachor <przemyslaw.czachor@fingoweb.com>
2023-10-17 13:58:35 +0200

    Added basic eslint, and webpack config

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+const plugins = ['@typescript-eslint', 'import', 'prettier'];
+const extend = [
+  'prettier',
+  'plugin:@typescript-eslint/recommended',
+  'plugin:import/typescript',
+];
+
+const rules = {
+  quotes: [2, 'single', { avoidEscape: true }],
+  semi: [2, 'always'],
+  'prettier/prettier': [
+    'error',
+    {
+      semi: true,
+      singleQuote: true,
+    },
+  ],
+
+  'import/prefer-default-export': 0,
+};
+
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  rules,
+  extends: extend,
+  plugins,
+};
