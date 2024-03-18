a55fdbf587e5d5b0de03b24756f70364cad3bc9e
Dariusz Piwko <dariusz.piwko@ringieraxelspringer.pl>
2024-03-18 13:54:22 +0100

    sponsored product styles

--- src/consts/tags.ts
+++ src/consts/tags.ts
@@ -1,15 +1,13 @@
 const TAG_LIST_CLASSNAME = 'tags';
-const TAG_ITEM_CLASSNAME = 'tags';
+const TAG_ITEM_CLASSNAME = 'promo new rmn-tag-spons';
 const TAG_TEXT_MARK_PL = 'sponsorowane';
 const TAG_TEXT_MARK_EN = 'sponsored';
 
 const ITEM_STYLES = {
   'margin-top': '4px',
-  'background-color': 'white',
-  color: 'black',
-  opacity: '0.5',
-  padding: '0',
+  margin: '2px',
   'z-index': '1000',
+  position: 'relative'
 };
 
 const LIST_STYLES = {
@@ -18,17 +16,14 @@ const LIST_STYLES = {
   marginBottom: '10px',
 };
 
-const ITEM_STYLES_PRODUCT_PAGE = {
-  'background-color': 'rgba(255,255,255,0.85)',
-  'text-transform': 'lowercase',
-  'font-weight': 'normal',
-  color: 'rgba(0,0,0,0.5)',
-  'letter-spacing': '0',
-  'line-height': '1em',
-  padding: '0',
-  'font-size': '11px',
-  position: 'relative',
-};
+const pseudoClassName = TAG_ITEM_CLASSNAME.split(' ').join('.');
+
+const PSEUDOCLASS_STYLES = `
+    li.${pseudoClassName}::before,
+    li.${pseudoClassName}::after {
+      display: none !important;
+    }
+  `;
 
 const MEDIA_QUERIES_TAG_PRODUCT_PAGE = `
 @media screen and (min-width: 768px) {
@@ -51,6 +46,6 @@ export {
   TAG_TEXT_MARK_EN,
   TAG_LIST_CLASSNAME,
   TAG_ITEM_CLASSNAME,
-  ITEM_STYLES_PRODUCT_PAGE,
+  PSEUDOCLASS_STYLES,
   MEDIA_QUERIES_TAG_PRODUCT_PAGE,
 };
--- src/utils/product/markProductAsPromoted.ts
+++ src/utils/product/markProductAsPromoted.ts
@@ -6,8 +6,7 @@ import {
   TAG_LIST_CLASSNAME,
   TAG_TEXT_MARK_PL,
   TAG_TEXT_MARK_EN,
-  ITEM_STYLES_PRODUCT_PAGE,
-  MEDIA_QUERIES_TAG_PRODUCT_PAGE,
+  MEDIA_QUERIES_TAG_PRODUCT_PAGE, PSEUDOCLASS_STYLES
 } from 'consts/tags';
 import { TPages } from 'types/pages';
 import applyStyles from 'utils/helpers/applyStyles';
@@ -19,12 +18,11 @@ const markProductAsPromoted = (product: HTMLElement, page: TPages) => {
   applyStyles(tagItem, ITEM_STYLES);
 
   if (page === PRODUCT_PAGE) {
-    const styles = document.createElement('style');
-    styles.innerHTML = MEDIA_QUERIES_TAG_PRODUCT_PAGE;
-    product.appendChild(styles);
+    const mediaQueryStyles = document.createElement('style');
+    mediaQueryStyles.innerHTML = MEDIA_QUERIES_TAG_PRODUCT_PAGE;
+    product.appendChild(mediaQueryStyles);
 
     applyStyles(tagsList, LIST_STYLES);
-    applyStyles(tagItem, ITEM_STYLES_PRODUCT_PAGE);
   }
 
   const locale = Shop.lang.name ? Shop.lang.name : 'pl_PL';
@@ -35,7 +33,12 @@ const markProductAsPromoted = (product: HTMLElement, page: TPages) => {
   tagsList.className = TAG_LIST_CLASSNAME;
   tagItem.className = TAG_ITEM_CLASSNAME;
 
+  const styles = document.createElement('style');
+  styles.innerHTML = PSEUDOCLASS_STYLES;
+  tagsList.appendChild(styles);
+
   tagsList.appendChild(tagItem);
+
   product.prepend(tagsList);
 
   return product;
